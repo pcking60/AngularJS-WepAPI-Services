77fa46d753bb4389d68686e88046b1fec369a6f5
pcking60 <thientt.bdst@gmail.com>
2017-04-01 14:03:29 +0700

    initial migrations

--- TedShop.Data/Repositories/ProductCaterogyRepository.cs
+++ TedShop.Data/Repositories/ProductCaterogyRepository.cs
@@ -1,14 +1,26 @@
-﻿using TeduShop.Data.Infrastructure;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeduShop.Data.Infrastructure;
 using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    public class ProductCaterogyRepository: RepositoryBase<ProductCategory>
+    public interface IProductCategoryRepository
     {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+    public class ProductCaterogyRepository: RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        
         public ProductCaterogyRepository(IDBFactory dbFactory): base(dbFactory)
         {
 
         }
 
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
     }
 }
---
+++ TedShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TedShop.Data.Repositories
+{
+    public interface IProductRepository { }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDBFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
