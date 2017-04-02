c21b7f201aa7e2238788602ec3009195acf2ff64
pcking60 <thientt.bdst@gmail.com>
2017-04-02 09:37:57 +0700

    rebuild service

---
+++ TeduShop.Service/PostCategoryService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IPostCategoryService
+    {
+        void Add(PostCategory postCategory);
+        void update(PostCategory postCategory);
+        void delete(int id);
+        IEnumerable<PostCategory> GetAll();
+        IEnumerable<PostCategory> GetAllByParentID(int parentID);
+        PostCategory GetByID(int id);
+    }
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategory, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategory;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentID(int parentID)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentID);
+        }
+
+        public PostCategory GetByID(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
